85f948a157af980f892c27d71b5f0e9d823d05b4
tysepa <tuyiepa88@gmail.com>
2022-03-09 18:40:36 +0200

    New authatication

---
+++ app.js
@@ -0,0 +1,55 @@
+import express from'express';
+import jwt from'jsonwebtoken';
+
+const app = express();
+
+app.get('/api',(req, res)=>{
+    res.json({
+        message:'welcome on the API'
+    });
+});
+
+app.post('/api/posts',verifyToken,(req, res)=>{
+    jwt.verify(req.token, 'secretkey',(err, authData)=>{
+        if(err){
+            res.sendStatus(403);
+        }else{
+
+            res.json({
+                message: 'Post create...',
+                authData
+            });
+        }
+    })
+});
+
+app.post('/api/login',(req, res)=>{
+    const user ={
+        id:1,
+        username:'Epa',
+        email:'epa@gmail.com'
+    }
+
+    jwt.sign({user}, 'secretkey',(err, token)=>{
+        res.json({
+            token
+        });
+    });
+});
+
+function verifyToken(req, res, next){
+    const beareHeader = req.headers['authorization']; 
+     
+    if( typeof beareHeader !=='undefined'){
+       const bearer = beareHeader.split(' ');
+       const bearerToken = bearer[1];
+       req.token = bearerToken;
+       next();
+    }else{
+        res.sendStatus(403);
+    }
+}
+
+app.listen(5000,(req, res)=>{
+    console.log("Server started on port 5000"); 
+})
